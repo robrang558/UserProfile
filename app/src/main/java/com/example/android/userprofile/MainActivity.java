64b235d61cc0489c67c62507c7c539f771a550df
rrangel <THI$i$@GHpw1!>
2018-05-20 14:29:41 -0500

    Moved ImageView to top of Java for better readability.

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -11,16 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView profileImageView = findViewById(R.id.profile_picture);
+        profileImageView.setImageResource(R.drawable.lollipop);
+        
         TextView nameTextView = findViewById(R.id.name);
         nameTextView.setText("Robert");
         nameTextView.setTextSize(50);
+
         TextView birthdayTextView = findViewById(R.id.birthday);
         birthdayTextView.setText("April 30");
         birthdayTextView.setTextSize(25);
+
         TextView countryTextView = findViewById(R.id.country);
         countryTextView.setText("USA");
         countryTextView.setTextSize(20);
-        ImageView profileImageView = findViewById(R.id.profile_picture);
-        profileImageView.setImageResource(R.drawable.lollipop);
     }
 }
